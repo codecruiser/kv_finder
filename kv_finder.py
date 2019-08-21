7f900208d8c0687c3058ed454efc3ae9f0536ba7
KIHGFF <ggg@ddd.dd>
2019-08-21 23:13:06 +0200

    search structure check , gitignore added, venv removed

--- kv_finder.py
+++ kv_finder.py
@@ -13,18 +13,48 @@ class KVFinder:
     keys or path build from keys.
     """
 
-    def __init__(self, search_struct=None, search_keys=None):
-        self.search_struct = search_struct
+    def __init__(self, json_struct=None, search_keys: list=None):
+        """
+        Initiate KeyValueFinder with structure to search within and with
+        searched .
+
+        :param search_struct: structure to search within
+        :param search_keys: keys searched, either as list or keys or paths
+        """
+        self.json_struct = json_struct
         self.search_keys = search_keys
 
-    def set_struct(self, search_struct):
-        self.search_struct = search_struct
+    def set_struct(self, json_struct):
+        self.json_struct = json_struct
 
     def set_searched(self, search_keys):
         self.search_keys = search_keys
 
+    def check_object(self, current_object):
+        if isinstance(current_object, (list, tuple)):
+            for item in current_object:
+                self.check_object(item)
+        elif isinstance(current_object, dict):
+            for key, item in current_object:
+                self.check_object(item)
+        else:
+            pass
+
     def search(self):
-        pass
+        """
+        Starting point for search.
+        """
+        if not self.json_struct:
+            raise Exception(
+                "Structure on which search should be performed is not provided"
+            )
+        if not self.search_keys:
+            raise Exception("Search keys are not provided.")
+
+        self.check_object(self.json_struct)
 
     def __iter__(self):
+        """
+        Iterates over searched keys.
+        """
         print("A")
